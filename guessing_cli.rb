6ff06e25f3cda0a4c8d21e28ae5a609bf913597b
Michelle Blevins <mjblevins7@gmail.com>
2018-04-15 15:22:12 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,22 +2,25 @@
 require "pry"
 
 def run_guessing_game
+  comp_input = rand(1..6)
   user_input = ""
   #comp_input = rand(1..6)
   while user_input
     puts "Guess a number between 1 and 6."
     user_input = gets.chomp
-    comp_input = rand(1..6)
-    binding.pry
+    
+    #binding.pry
+    if user_input.downcase == "exit"
+      puts "Goodbye!"
+      break
+    else
       if user_input.to_i == comp_input
         puts "You guessed the correct number!"
-        return "You guessed the correct number!"
-      elsif user_input.downcase == "exit"
-        puts "Goodbye!"
-        return "Goodbye!"
+        break
       else
         "The computer guessed #{comp_input}."
       end
     end
+  end
         #binding.pry
 end
